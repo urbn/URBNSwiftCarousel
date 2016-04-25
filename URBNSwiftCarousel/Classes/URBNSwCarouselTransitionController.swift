f616aefb25f49515672479fd0541875747bfe45f
Kevin Taniguchi <ktaniguchi@urbn.com>
2016-04-25 17:43:13 -0400

    began transition controller

---
+++ URBNSwiftCarousel/Classes/URBNSwCarouselTransitionController.swift
@@ -0,0 +1,25 @@
+//
+//  URBNSwCarouselTransitionController.swift
+//  Pods
+//
+//  Created by Kevin Taniguchi on 4/25/16.
+//
+//
+
+import UIKit
+
+class URBNSwCarouselTransitionController: NSObject, UIViewControllerAnimatedTransitioning, UIViewControllerTransitioningDelegate {
+    
+    func transitionDuration(transitionContext: UIViewControllerContextTransitioning?) -> NSTimeInterval {
+        // TODO update
+        return 1
+    }
+    
+    func animateTransition(transitionContext: UIViewControllerContextTransitioning) {
+        
+    }
+    
+    func animationEnded(transitionCompleted: Bool) {
+        
+    }
+}
