72fb2e94ac28736b0cd5da44d938ead35dac17ac
Kevin Taniguchi <ktaniguchi@urbn.com>
2016-04-27 22:21:21 -0400

    clean master for review

--- Example/URBNSwiftCarousel/Convenience.swift
+++ Example/URBNSwiftCarousel/Convenience.swift
@@ -6,15 +6,4 @@
 //  Copyright © 2016 CocoaPods. All rights reserved.
 //
 
-import UIKit
-
-extension UIColor {
-    static var colors: [UIColor] {
-        return [UIColor.redColor(), UIColor.orangeColor(), UIColor.purpleColor(), UIColor.cyanColor(), UIColor.blueColor(), UIColor.greenColor(), UIColor.yellowColor()]
-    }
-    
-    static func colorForIndex(index: Int) -> UIColor {
-        return colors[index]
-    }
-}
 
